a5dbc7e418ade0bca4aacd9fcc046fc0ed19a20e
yeqianfeng <yeqianfeng911@163.com>
2015-05-10 14:43:55 +0800

    version 1.0

---
+++ Test.java
@@ -0,0 +1,12 @@
+/*************************************************************************
+    > File Name: Test.java
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: Sun 10 May 2015 02:30:31 PM CST
+ ************************************************************************/
+public class Test{
+
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
